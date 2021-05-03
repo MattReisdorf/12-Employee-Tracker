165232e3a15dca570098d6892d0584a55a5e775f
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-05-02 22:45:57 -0400

    viewAllEmployees function and endConnection on Exit

--- index.js
+++ index.js
@@ -2,12 +2,35 @@
 const password = require('./dbpassword.json');
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const table = require('console.table');
+const { first } = require('lodash');
 
 
+const initalPrompt = {
+    type: 'list',
+    name: 'firstResponse',
+    message: 'What Would You Like To Do?',
+    choices: ['View All Employees',
+            'View Employees By Department',
+            'View Employees By Manager',
+            'Add an Employee',
+            'Remove an Employee',
+            'Add a Role',
+            'Remove a Role',
+            'Add a Department',
+            'Remove a Department',
+            `Update an Employee's Role`,
+            `Update an Employee's Manager`,
+            'Exit']
+};
 
 
-
-
+// const bsPrompt = {
+//     type: 'list',
+//     name: 'bsPrompt',
+//     message: 'waht now?',
+//     choices: ['exit','go back to beginning']
+// }
 
 
 
@@ -22,7 +45,7 @@ const connection = mysql.createConnection({
     // Username
     user: 'root',
 
-    // Password
+    // Password: Password is Hidden in Ignored JSON
     password: password.password,
     
     // Database
@@ -32,4 +55,79 @@ const connection = mysql.createConnection({
 // Connect to the DB
 connection.connect((err) => {
     err ? console.error(err) : console.log('Connected to Database');
+    firstPrompt();  
 });
+
+
+// Functions
+const endConnection = () => {
+    connection.end();
+};
+
+const firstPrompt = () => {
+    inquirer
+        .prompt(initalPrompt)
+        .then((initalPrompt) => {
+            switchCases(initalPrompt.firstResponse);
+            // endConnection(); // end connection, remove before deploying
+        })
+}
+
+const switchCases = (responses) => {
+    switch(responses) {
+        case 'View All Employees':
+            // console.log('View All Employees');
+            viewAllEmployees();
+            break;
+        case 'View Employees By Department':
+            console.log('View Employees By Department');
+            // viewByDepartment();
+            break;
+        case 'View Employees By Manager':
+            console.log('View Employees By Manager');
+            // viewByManager();
+            break;
+        case 'Add an Employee':
+            console.log('Add an Employee');
+            break;
+        case 'Remove an Employee':
+            console.log('Remove an Employee');
+            break;
+        case 'Add a Role':
+            console.log('Add a Role');
+            break;
+        case 'Remove a Role':
+            console.log('Remove a Role');
+            break;
+        case 'Add a Department':
+            console.log('Add a Department');
+            break;
+        case 'Remove a Department':
+            console.log('Remove a Department');
+            break;
+        case `Update an Employee's Role`:
+            console.log(`Update an Employee's Role`);
+            break;
+        case `Update an Emplyoee's Manager`:
+            console.log(`Update an Employee's Manager`);
+            break;
+        case `Exit`:
+            // console.log(`Exit`);
+            endConnection();
+            break;
+    };
+};
+
+const viewAllEmployees = () => {
+    connection.query(
+        `SELECT * FROM Employees`,
+        (err, res) => {
+            err ? console.error(err) : console.table(res);
+            firstPrompt();
+        }
+    )
+}
+
+
+
+
