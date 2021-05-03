efb368562e059d1f6a661ebe36898be1f41ce0ed
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-05-03 11:41:29 -0400

    add employee functions, still need to do sql stuff

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const inquirer = require('inquirer');
 const table = require('console.table');
 
 
+// Other Stuff
+let existingRoles = new Array;
+
+let existingManagers = new Array;
+
+
+
+
+
+
+// Inquirer Prompts
 const initalPrompt = {
     type: 'list',
     name: 'firstResponse',
@@ -22,6 +33,44 @@ const initalPrompt = {
             `Update an Employee's Manager`,
             'Exit']
 };
+
+const addEmployeeNamePrompts = [
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'What Is Their First Name?'
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: 'What Is Their Last Name?'
+    }
+];
+
+const existingOrNewRolePrompt = {
+    type: 'list',
+    name: 'existingOrNew',
+    message: 'Is This an Existing or New Role?',
+    choices: ['Existing', 'New']
+}
+
+const newRolePrompt = {
+    type: 'input',
+    name: 'newRole',
+    message: `What Is This Employee's Role?`
+}
+
+const existingRolePrompt = {
+    type: 'list',
+    name: 'existingRole',
+    message: `What Is This Employee's Role`,
+    choices: existingRoles
+}
+
+
+
+
+
 
 
 // DB Connection
@@ -48,6 +97,14 @@ connection.connect((err) => {
 });
 
 
+
+
+
+
+
+
+
+
 // Functions
 const endConnection = () => {
     connection.end();
@@ -71,11 +128,10 @@ const switchCases = (responses) => {
             viewByDepartment();
             break;
         case 'View Employees By Manager':
-            console.log('View Employees By Manager');
-            // viewByManager();
+            viewByManager();
             break;
         case 'Add an Employee':
-            console.log('Add an Employee');
+            addEmployeeName();
             break;
         case 'Remove an Employee':
             console.log('Remove an Employee');
@@ -126,5 +182,90 @@ const viewByDepartment = () => {
     );
 };
 
+const viewByManager = () => {
+    // NEEDS JOIN
+    connection.query(
+        `SELECT * FROM Employees WHERE ?`,
+        {
+            Manager_ID: 1 || 2
+        },
+        (err,res) => {
+            err ? console.error(err) : console.table(res);
+            init();
+        }
+    )
+}
+
+const addEmployeeName = () => {
+    inquirer
+        .prompt(addEmployeeNamePrompts)
+        .then((addEmployeeNamePrompts) => {
+            // console.log(addEmployeeNamePrompts.firstName, addEmployeeNamePrompts.lastName);
+            addEmployeeRole(addEmployeeNamePrompts.firstName, addEmployeeNamePrompts.lastName);
+        }
+    )
+}
+
+const addEmployeeRole = (firstName, lastName) => {
+    connection.query(
+        // Get Existing Roles from DB
+        `SELECT Title FROM Roles`,
+        (err, res) => {
+            if (err) {
+                console.error(err);
+            };
+            // For Loop to Populate Existing Roles from DB
+            for (let i = 0; i < res.length; i++) {
+                if (existingRoles.includes(res[i].Title)) {
+                    continue;
+                }
+                else {
+                    existingRoles.push(res[i].Title);
+                }
+            }
+            existingOrNewRole(firstName, lastName);
+        }
+    )
+}
+
+const existingOrNewRole = (firstName, lastName) => {
+    inquirer
+        .prompt(existingOrNewRolePrompt)
+        .then((existingOrNewRolePrompt) => {
+            if (existingOrNewRolePrompt.existingOrNew == 'Existing') {
+                existingRole(firstName, lastName);
+            }
+            else if (existingOrNewRolePrompt.existingOrNew == 'New') {
+                // console.log('New Role');
+                newRole(firstName, lastName);
+            }
+            // console.log(firstName, lastName); // Check to make sure previous prompt data is still coming through new functions
+            
+        }
+    )
+}
 
 
+const newRole = (firstName, lastName) => {
+    inquirer
+        .prompt(newRolePrompt)
+        .then((newRolePrompt) => {
+            // console.log(firstName, lastName, newRolePrompt.newRole);
+            addEmployeeManager(firstName, lastName, newRolePrompt.newRole);
+        })
+}
+
+const existingRole = (firstName, lastName) => {
+    inquirer
+        .prompt(existingRolePrompt)
+        .then((existingRolePrompt) => {
+            // console.log(firstName, lastName, existingRolePrompt.existingRole);
+            addEmployeeManager(firstName, lastName, existingRolePrompt.existingRole);
+        })
+}
+
+const addEmployeeManager = (firstName, lastName, role) => {
+    console.log('function call worked');
+    console.log(firstName, lastName, role);
+    init();
+}
