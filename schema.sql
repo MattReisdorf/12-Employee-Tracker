8cde05e7cea4db7928e8ec17b92056f2b8ecfe1f
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-05-04 20:42:44 -0400

    updated sql stuff

--- schema.sql
+++ schema.sql
@@ -5,13 +5,13 @@ CREATE DATABASE All_Employees_DB;
 USE All_Employees_DB;
 
 CREATE TABLE Departments (
-	Department_ID INTEGER NOT NULL AUTO_INCREMENT,
+    Department_ID INTEGER NOT NULL AUTO_INCREMENT,
     Department_Name VARCHAR(30),
     PRIMARY KEY (Department_ID)
 );
 
 CREATE TABLE Roles (
-	Role_ID INTEGER NOT NULL AUTO_INCREMENT,
+    Role_ID INTEGER NOT NULL AUTO_INCREMENT,
     Title VARCHAR(30) NOT NULL,
     Salary DECIMAL(12,2) NOT NULL, 
     Department_ID INTEGER NOT NULL,
@@ -20,16 +20,18 @@ CREATE TABLE Roles (
 );
 
 CREATE TABLE Managers (
-	Manager_ID INTEGER NOT NULL AUTO_INCREMENT,
+    Manager_ID INTEGER NOT NULL AUTO_INCREMENT,
     First_Name VARCHAR(30) NOT NULL, 
     Last_Name VARCHAR(30) NOT NULL, 
     Role_ID INTEGER NOT NULL, 
     CONSTRAINT fk_Manager_Role_ID FOREIGN KEY (Role_ID) REFERENCES Roles(Role_ID),
+    Department_ID INTEGER NOT NULL,
+    CONSTRAINT fkManager_Department_ID FOREIGN KEY (Department_ID) REFERENCES Departments(Department_ID),
     PRIMARY KEY (Manager_ID)
 );
 
 CREATE TABLE Employees (
-	Employee_ID INTEGER NOT NULL AUTO_INCREMENT,
+    Employee_ID INTEGER NOT NULL AUTO_INCREMENT,
     First_Name VARCHAR(30),
     Last_Name VARCHAR(30),
     Role_ID INTEGER NOT NULL, 
--- seed.sql
+++ seed.sql
@@ -12,8 +12,8 @@ VALUES ('Drone', 20000, 1);
 INSERT INTO Roles(Title, Salary, Department_ID)
 VALUES ('Drone Manager', 60000, 2);
 
-INSERT INTO Managers(First_Name, Last_Name, Role_ID)
-VALUES('Droney', 'McDroneface', 2);
+INSERT INTO Managers(First_Name, Last_Name, Role_ID, Department_ID)
+VALUES('Droney', 'McDroneface', 2, 1);
 
 INSERT INTO Employees(First_Name, Last_Name, Role_ID)
 VALUES ('Droney', 'McDroneface', 2);
@@ -21,7 +21,6 @@ VALUES ('Droney', 'McDroneface', 2);
 INSERT INTO Employees(First_Name, Last_Name, Role_ID, Manager_ID)
 VALUES ('Smitty', 'Werbenjagermanjensen', 1, 1);
 
-
 -- SELECT * FROM Departments;
--- SELECT * FROM Roles;
 -- SELECT * FROM Employees;
+-- SELECT * FROM Roles;
